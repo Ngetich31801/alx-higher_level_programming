bcccf68569e45cf091cdafd0c74d8d5a8c7ab932
Ngetich31801 <kipngetichjerry@gmail.com[F>
2023-06-07 05:04:57 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-module.export=class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-    }
-    this.width = w;
-    this.height = h;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-}
-
+};
