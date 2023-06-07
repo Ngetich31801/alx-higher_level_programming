1377f480a8a339969041b57c7c16b78582195c69
Ngetich31801 <kipngetichjerry@gmail.com[F>
2023-06-07 05:37:12 -0700

    task 0,3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      // Create an empty object if w or h is 0 or not a positive integer
-      this.width = 0;
-      this.height = 0;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    if (this.width === 0 || this.height === 0) {
-      console.log("Empty rectangle");
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log("X".repeat(this.width));
-      }
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
